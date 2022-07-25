d3fc2d6e52299ef6ae91d6479a98babab9d59366
Alan Pecenkovic <alanquad@yahoo.com>
2022-07-25 16:30:23 +0200

    styling components for android and iOS

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import MainButton from "../components/MainButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       <MainButton>Reset</MainButton>
       <MainButton>Confirm</MainButton>
     </View>
@@ -13,3 +13,29 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "crimson",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ceba06",
+    borderBottomWidth: 2,
+    color: "#ceba06",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
