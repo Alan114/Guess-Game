4df926ddebc5fd27a5baedbed60d1ef365c05f57
Alan Pecenkovic <alanquad@yahoo.com>
2022-07-26 13:55:09 +0200

    GameScreen layout and SafeAreaView component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const GameScreen = () => {
   return (
-    <View>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Opponent's guess</Text>
+      <Text>GUESS</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+        <Text>- +</Text>
+      </View>
+      <View>
+        <Text>Log rounds</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 25,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "gold",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "gold",
+    padding: 12,
+  },
+});
 
 export default GameScreen;
