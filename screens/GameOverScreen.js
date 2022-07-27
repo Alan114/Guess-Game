1867dd6baa5f243b652c28ff885b60fbadb1602b
Alan Pecenkovic <alanquad@yahoo.com>
2022-07-27 16:25:41 +0200

    Adding an image to the game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,43 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, View, Image, Text } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 const GameOverScreen = () => {
-  return <Text style={styles.text}>GAME OVER !!!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER !!!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y</Text>
+    </View>
+  );
 };
 
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 38,
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary400,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
